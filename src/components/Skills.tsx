29d99df0734dd9d676cd8c32c6e2567771a9a29b
Malak Sabry <malaksabry367@gmail.com>
2025-08-07 01:07:50 +0300

    ddd

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,4 +1,3 @@
-
 const skills = [
   "C++",
   "JavaScript",
@@ -13,10 +12,11 @@ const skills = [
   "MySQL",
   "npm",
   "Vite",
-  "GitHub",
+  "Git & GitHub",
   "Figma",
   "Photoshop",
   "Illustrator",
+  "Supabase",
 ];
 
 const SkillButton = ({ skill }: { skill: string }) => (
@@ -51,78 +51,77 @@ const SkillsSection = () => {
 const TechStack = () => {
   return (
     <section id="skills">
-<div className="dark:bg-[#0f0017] md:max-w-5xl lg:max-w-6xl mx-auto flex flex-col">
-      <div className="flex flex-col gap-36">
-        <div className="flex flex-col">
-          <div className="mb-0 sm:mb-12 mt-36">
-            <section className="flex flex-col text-center w-full justify-center items-center gap-6 md:gap-10 px-4">
-              <div className="max-w-lg text-center flex justify-center items-center flex-col gap-2">
-                <p className="text-2xl md:text-4xl font-ysabeau text-purple-500">
-                  SKILLS
-                </p>
-                <p className="text-sm md:text-lg lg:text-xl  text-purple-200 font-ubuntu font-medium">
-                  My key expertises and technical proficiencies
-                </p>
-              </div>
-            </section>
-          </div>
-
-          <div className="flex relative max-h-[429.44px] min-h-[429.44px] h-[60vh] flex-col items-center justify-center overflow-hidden dark:bg-[#0f0017] w-full rounded-md z-0">
-            {/* Background Gradients & Overlays */}
-            <div className="hidden sm:flex relative items-center justify-center isolate z-0">
-              {/* Left Gradient */}
-              <div
-                className="hidden sm:block absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-gradient-conic from-purple-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]"
-                style={{
-                  backgroundImage:
-                    "conic-gradient(var(--conic-position), var(--tw-gradient-stops))",
-                  opacity: 1,
-                  width: "30rem",
-                }}
-              >
-                <div className="hidden sm:block absolute w-full left-0 dark:bg-[#0f0017] h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
-                <div className="hidden sm:block absolute w-40 h-full left-0 dark:bg-[#0f0017] bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
-              </div>
-
-              {/* Right Gradient */}
-              <div
-                className="hidden sm:block absolute inset-auto left-1/2 h-56 w-[30rem] bg-gradient-conic from-transparent via-transparent to-purple-500 text-white [--conic-position:from_290deg_at_center_top]"
-                style={{
-                  backgroundImage:
-                    "conic-gradient(var(--conic-position), var(--tw-gradient-stops))",
-                  opacity: 1,
-                  width: "30rem",
-                }}
-              >
-                <div className="hidden sm:block absolute w-40 h-full right-0 dark:bg-[#0f0017] bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
-                <div className="hidden sm:block absolute w-full right-0 dark:bg-[#0f0017] h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
-              </div>
-
-              {/* Background Overlays */}
-              <div className="hidden sm:block absolute top-1/2 h-48 w-full translate-y-12 dark:bg-[#0f0017] blur-2xl" />
-              <div className="hidden sm:block absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md" />
-              <div className="hidden sm:block absolute inset-auto z-50 h-36 w-[28rem] -translate-y-1/2 rounded-full bg-purple-500 opacity-50 blur-3xl" />
-              <div
-                className="hidden sm:block absolute inset-auto z-30 h-36 w-64 -translate-y-[6rem] rounded-full bg-purple-400 blur-2xl"
-                style={{ width: "16rem" }}
-              />
-              <div
-                className="hidden sm:block top-[-30px] absolute inset-auto z-50 h-0.5 w-[30rem] -translate-y-[7rem] bg-purple-400"
-                style={{ width: "15rem" }}
-              />
-              <div className="hidden sm:block absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-slate-50 dark:bg-[#0f0017]" />
+      <div className="dark:bg-[#0f0017] md:max-w-5xl lg:max-w-6xl mx-auto flex flex-col">
+        <div className="flex flex-col gap-36">
+          <div className="flex flex-col">
+            <div className="mb-0 sm:mb-12 mt-36">
+              <section className="flex flex-col text-center w-full justify-center items-center gap-6 md:gap-10 px-4">
+                <div className="max-w-lg text-center flex justify-center items-center flex-col gap-2">
+                  <p className="text-2xl md:text-4xl font-ysabeau text-purple-500">
+                    SKILLS
+                  </p>
+                  <p className="text-sm md:text-lg lg:text-xl  text-purple-200 font-ubuntu font-medium">
+                    My key expertises and technical proficiencies
+                  </p>
+                </div>
+              </section>
             </div>
 
-            {/* Skills Buttons Section */}
-            <div className="relative z-50 flex -translate-y-4 flex-col items-center px-5">
-              <SkillsSection />
+            <div className="flex relative max-h-[429.44px] min-h-[429.44px] h-[60vh] flex-col items-center justify-center overflow-hidden dark:bg-[#0f0017] w-full rounded-md z-0">
+              {/* Background Gradients & Overlays */}
+              <div className="hidden sm:flex relative items-center justify-center isolate z-0">
+                {/* Left Gradient */}
+                <div
+                  className="hidden sm:block absolute inset-auto right-1/2 h-56 overflow-visible w-[30rem] bg-gradient-conic from-purple-500 via-transparent to-transparent text-white [--conic-position:from_70deg_at_center_top]"
+                  style={{
+                    backgroundImage:
+                      "conic-gradient(var(--conic-position), var(--tw-gradient-stops))",
+                    opacity: 1,
+                    width: "30rem",
+                  }}
+                >
+                  <div className="hidden sm:block absolute w-full left-0 dark:bg-[#0f0017] h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
+                  <div className="hidden sm:block absolute w-40 h-full left-0 dark:bg-[#0f0017] bottom-0 z-20 [mask-image:linear-gradient(to_right,white,transparent)]" />
+                </div>
+
+                {/* Right Gradient */}
+                <div
+                  className="hidden sm:block absolute inset-auto left-1/2 h-56 w-[30rem] bg-gradient-conic from-transparent via-transparent to-purple-500 text-white [--conic-position:from_290deg_at_center_top]"
+                  style={{
+                    backgroundImage:
+                      "conic-gradient(var(--conic-position), var(--tw-gradient-stops))",
+                    opacity: 1,
+                    width: "30rem",
+                  }}
+                >
+                  <div className="hidden sm:block absolute w-40 h-full right-0 dark:bg-[#0f0017] bottom-0 z-20 [mask-image:linear-gradient(to_left,white,transparent)]" />
+                  <div className="hidden sm:block absolute w-full right-0 dark:bg-[#0f0017] h-40 bottom-0 z-20 [mask-image:linear-gradient(to_top,white,transparent)]" />
+                </div>
+
+                {/* Background Overlays */}
+                <div className="hidden sm:block absolute top-1/2 h-48 w-full translate-y-12 dark:bg-[#0f0017] blur-2xl" />
+                <div className="hidden sm:block absolute top-1/2 z-50 h-48 w-full bg-transparent opacity-10 backdrop-blur-md" />
+                <div className="hidden sm:block absolute inset-auto z-50 h-36 w-[28rem] -translate-y-1/2 rounded-full bg-purple-500 opacity-50 blur-3xl" />
+                <div
+                  className="hidden sm:block absolute inset-auto z-30 h-36 w-64 -translate-y-[6rem] rounded-full bg-purple-400 blur-2xl"
+                  style={{ width: "16rem" }}
+                />
+                <div
+                  className="hidden sm:block top-[-30px] absolute inset-auto z-50 h-0.5 w-[30rem] -translate-y-[7rem] bg-purple-400"
+                  style={{ width: "15rem" }}
+                />
+                <div className="hidden sm:block absolute inset-auto z-40 h-44 w-full -translate-y-[12.5rem] bg-slate-50 dark:bg-[#0f0017]" />
+              </div>
+
+              {/* Skills Buttons Section */}
+              <div className="relative z-50 flex -translate-y-4 flex-col items-center px-5">
+                <SkillsSection />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </section>
-    
   );
 };
 
